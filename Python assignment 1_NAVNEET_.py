f8c1ad3e8b9c05b96dd2cd9a6d598daba574d62a
Navneet-11 <70103750+Navneet-11@users.noreply.github.com>
2020-09-05 19:59:33 +0530

    Day 2 Assignment Jupiter Notebook

---
+++ Python assignment 1_NAVNEET_.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 1st = ["Navneet", 10, 3214, 124.24, [1,2,3]
+# 
+
+# In[2]:
+
+
+1
+
+
+# In[3]:
+
+
+# Floating numbers / Float
+
+c = 7.1
+d = 8.6
+e = 7.0
+
+
+# In[4]:
+
+
+c + d
+
+
+# In[5]:
+
+
+type (e)
+
+
+# In[10]:
+
+
+name = "Navneet Waghmare "
+name1 = "Eyecandy"
+
+
+# In[7]:
+
+
+name1
+
+
+# In[9]:
+
+
+name + "space" +name1
+
+
+# In[11]:
+
+
+type (name)
+
+
+# In[12]:
+
+
+lst = []"Navneet", 10, 3214, 124.24, [1,2,3]]
+
+
+# In[13]:
+
+
+2
+
+
+# In[15]:
+
+
+# Data is stored like ->
+
+# {"KEY": "Value" }
+
+
+# In[16]:
+
+
+dit = {"name":"Navneet", "age":"19",
+       "number":123456,"email":"navneetwaghmare3721@gmail.com"}
+
+
+# In[17]:
+
+
+dit
+
+
+# In[18]:
+
+
+dit.get("name")
+
+
+# In[19]:
+
+
+dit["name"]
+
+
+# In[ ]:
+
+
+
+
